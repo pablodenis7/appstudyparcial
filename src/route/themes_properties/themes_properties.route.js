e13bcf2e6e86e36010bf91553000eefb1438a58d
Pablo Denis <42759958+pablodenis7@users.noreply.github.com>
2023-03-31 20:30:00 -0400

    ultim

--- src/route/themes_properties/themes_properties.route.js
+++ src/route/themes_properties/themes_properties.route.js
@@ -4,4 +4,5 @@ module.exports = function (app){
     app.get("/themes_properties/list", themes_propertiesController.listar);
     app.get("/themes_properties/:id", themes_propertiesController.consultarPorCodigo);
     app.put("/themes_properties/update", themes_propertiesController.actualizar);
-   app.delete("/themes_properties/delete/:id", themes_propertiesController.eliminar);}
+    app.delete("/themes_properties/delete/:id", themes_propertiesController.eliminar);
+}
